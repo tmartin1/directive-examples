b87c78f34d9667129c07bdb2427274b181e38ad1
Tim Martin <timothy.m.martin@gmail.com>
2015-02-14 17:32:51 -0800

    Fix simple example.

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 'use strict';
 
-var app = angular.module('exampleApp', ['ui.router'])
-.config(function($stateProvider, $urlRouterProvider, $locationProvider, $httpProvider) {
+var app = angular.module('exampleApp', ['ui.router']);
+// .config(function($stateProvider, $urlRouterProvider, $locationProvider, $httpProvider) {
 
-  $stateProvider
-  .state('simple', {
-    url: '/simple',
-    templateUrl: 'simple/simple.html',
-    controller: 'SimpleCtrl'
-  })
-  .state('animation', {
-    url: '/animation',
-    templateUrl: 'animation/animation.html',
-    controller: 'AnimationCtrl'
-  });
-});
+//   $stateProvider
+//   .state('simple', {
+//     url: '/simple',
+//     templateUrl: 'simple/simple.html',
+//     controller: 'SimpleCtrl'
+//   })
+//   .state('animation', {
+//     url: '/animation',
+//     templateUrl: 'animation/animation.html',
+//     controller: 'AnimationCtrl'
+//   });
+// });
--- simple.js
+++ simple.js
@@ -1,14 +1,14 @@
 'use strict';
 
 app.controller('SimpleCtrl', function($scope) {
-  //
+  $scope.name = 'Flash Gordon';
 });
 
 app.directive('repeater',function(){
   return {
     replace:false,
     restrict: 'E',
-    template: '<h3>Your name is {{name}}</h3>',
+    template: '<h3>Your name is: {{name}}</h3>',
     link: function() {
       //
     }
