e363ff2b971a92082cd6c6cc5cecc7e9922977c1
Tim Martin <timothy.m.martin@gmail.com>
2015-02-15 14:26:03 -0800

    Update animate exmple with outline.

--- animation.js
+++ animation.js
@@ -1,6 +1,17 @@
 'use strict';
 
 angular.module('exampleApp')
-.controller('AnimationCtrl', function($rootScope, $scope, $location, $state) {
-  //
+.controller('AnimationCtrl', function($scope) {
+  $scope.name = 'Flash Gordon';
+});
+
+app.directive('animator',function(){
+  return {
+    replace:false,
+    restrict: 'E',
+    template: '<h3>Your name is: {{name}}</h3>',
+    link: function() {
+      //
+    }
+  }
 });
