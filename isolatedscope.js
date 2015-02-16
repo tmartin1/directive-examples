4031b327155409bbb75135d5f35a5fd242160998
Tim Martin <timothy.m.martin@gmail.com>
2015-02-16 15:34:54 -0800

    Add example of isolated scope.

---
+++ isolatedscope.js
@@ -0,0 +1,24 @@
+angular.module('scopingApp', [])
+
+.controller('ScopingCtrl', function($scope) {
+  $scope.user = { name:'Default Name', age:'Default Age' };
+  $scope.query = [
+    { question: 'What is your name?', text:'Your name is: ' , model:'Default Name' },
+    { question: 'How old are you?', text:'Your age is: ' , model:'Default Age' }
+  ];
+})
+
+.directive('question',function(){
+  return {
+    scope: {
+      binding:'=',
+      query: '='
+    }, // Creates an independent scope for this directive, idenpendent of the parent scope.
+    replace:false,
+    restrict: 'AE',
+    template: '\
+      <h3>{{query.question}}</h3> \
+      <input type="text" ng-model="query.model"/> \
+      <h4><i>(From child scope)</i> {{query.text}}{{query.model}}</h4><br>'
+  }
+});
