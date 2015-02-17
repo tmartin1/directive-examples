b7a8dcd41bfc264805a0f2feed03c431b874411a
Tim Martin <timothy.m.martin@gmail.com>
2015-02-17 09:43:55 -0800

    Update dynamic example.

--- dynamic.js
+++ dynamic.js
@@ -3,59 +3,85 @@
 angular.module('dynamicApp', [])
 
 .controller('DynamicCtrl', function($scope) {
-  $scope.user = {};
-  $scope.items = [
-    { question: 'What is your name?', text: 'Your name is: ', bindTo: 'user.name' },
-    { question: 'How do you feel?', text: 'You feel: ', bindTo: 'user.feel' }
+  $scope.user = { name:'Flash Gordon', age:'26', sex:'male' };
+  $scope.queries = [
+    { title: 'name', question: 'What is your name? ', bind: 'name', type: 'text' },
+    { title: 'age', question: 'What is your age? ', bind: 'age', type: 'text' }
   ];
 })
 
-// Child scope variables change, but they are not bound to parent scope variables.
-// As such, they are not properly saved in the parent scope.
-// ****************************************************************************
-// .directive('repeater', function($rootScope) {
-//   return {
-//     scope: false, // true = Use a child scope that inherits from parent.
-//     replace: false,
-//     restrict: 'E',
-//     template: ' \
-//       <h3>{{item.question}}</h3> \
-//       <input type="text" ng-model="item.model" ng-bind="{{item.bindTo}}" /> \
-//       <h4>{{item.text}}{{item.model}}</h4><br>'
-//   };
-// });
-
-// Putting the ng-repeat inside the template creates a child scope for each repeat.
-// But this still does not solve the problem of saving variables to the parent scope. 
-// ****************************************************************************
-.directive('repeater', function($rootScope) {
+.directive('questions', function() {
   return {
-    scope: {
-      binding: '=',
-      item: '='
-    },
-    bindToController: true,
-    replace: false,
     restrict: 'E',
+    scope: {
+      queries:'=',
+      user: '='
+    },
+    controller: function($scope) {
+      console.log($scope);
+      // this.template = templates[$scope.query.type];
+    },
     template: ' \
-        <h3>{{item.question}}</h3> \
-        <input type="text" ng-model="item.bindTo" /> \
-        <h4>{{item.text}}{{item.model}}</h4><br> \
-      ',
-    compile: function(tElem, attrs) {
-      //do optional DOM transformation here
-      // console.log(tElem);
-      // console.log(attrs);
-      // console.log($('#'+scope.binding));
-      return function(scope, elem, attrs) {
-        console.log(scope);
-        console.log(elem[0].children[1]);
-        // attrs.$set('ng-model', scope.binding);
-        attrs.$set('repeater', null);
-        // console.log('Scope in compile:')
-        // console.log(scope.binding);
-        // console.log(elem[0]);
+      <my-tabs> \
+        <my-pane ng-repeat="query in queries" title="{{query.title}}"> \
+          <h4>{{query.question}}</h4> \
+          <question query="query" user="user" ng-model="user[query.bind]"></question> \
+        </my-pane> \
+      </my-tabs>'
+  };
+})
+
+.directive('question', function() {
+  return {
+    restrict: 'E',
+    transclude: true,
+    scope: {
+      query: '=',
+      user: '='
+    },
+    templateUrl: 'dynamic-template.html'
+  };
+})
+
+.directive('myTabs', function() {
+  return {
+    restrict: 'E',
+    transclude: true,
+    scope: {},
+    controller: function($scope) {
+      var panes = $scope.panes = [];
+      $scope.select = function(pane) {
+        angular.forEach(panes, function(pane) {
+          pane.selected = false;
+        });
+        pane.selected = true;
       };
-    }
+
+      this.addPane = function(pane) {
+        if (panes.length === 0) {
+          $scope.select(pane);
+        }
+        panes.push(pane);
+      };
+    },
+    templateUrl: 'dynamic-tabs.html'
+  };
+})
+
+.directive('myPane', function() {
+  return {
+    require: '^myTabs',
+    restrict: 'E',
+    transclude: true,
+    scope: {
+      title: '@'
+    },
+    link: function(scope, element, attrs, tabsCtrl) {
+      tabsCtrl.addPane(scope);
+    },
+    template: '<div class="tab-pane" ng-show="selected" ng-transclude></div>'
   };
 });
+
+
+
